e82cbd37e08bc15a776efd32793577da0ba98671
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-10 17:56:15 -0300

    terraform-lab

---
+++ Terraform-build/variavel.tf
@@ -0,0 +1,11 @@
+variable "network_name" {
+  description = "devops-rede"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "centro_custo_rh" {
+  description = "Rede-Departamento"
+  type        = string
+  default     = "rh"
+}
