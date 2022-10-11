5031adb59742855ccdf331dc274cc52bf2131972
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 11:05:55 -0300

    terraform build

--- network.tf
+++ network.tf
@@ -3,15 +3,15 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_compute_subnetwork" "subnet-with-logging" {
-  name          = "log-test-subnetwork"
+resource "google_compute_subnetwork" "subnet-frontend" {
+  name          = "frontend-vpc-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.custom-test.id
+  network       = google_compute_network.frontend-vpc.id
 
 }
 
-resource "google_compute_network" "custom-test" {
-  name                    = "log-test-network"
+resource "google_compute_network" "frontend-vpc" {
+  name                    = "frontend-infraestruitura"
   auto_create_subnetworks = false
 }
