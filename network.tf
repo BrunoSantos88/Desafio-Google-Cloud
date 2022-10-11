dcb8bf199e37ddc8aa6a48f812ddf4f6588662fb
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 00:03:16 -0300

    terraform build

--- network.tf
+++ network.tf
@@ -1,12 +1,13 @@
-resource "google_compute_subnetwork" "network-with-private-ip-ranges" {
-  name          = "rede-subnetwork"
-  ip_cidr_range = "192.10.0.0/24"
+resource "google_compute_subnetwork" "subnet-aplicaçao" {
+  name          = "coneçao-app-frontend"
+  ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.rede-america-norte.id
+  network       = google_compute_network.network-aplicaçao.id
 }
-  
 
-resource "google_compute_network" "rede-america-norte" {
-  name                    = "vpc-america-norte"
+resource "google_compute_network" "network-aplicaçao" {
+  project                 = "bootcamp-363315"
+  name                    = "test-network"
   auto_create_subnetworks = false
+  mtu                     = 1460
 }
