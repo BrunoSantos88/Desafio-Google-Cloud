a81a06775cf32270a6f7f4a03301d216a2e69f10
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 10:50:21 -0300

    terraform build

--- network.tf
+++ network.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-network"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
   mtu                     = 1460
 }
 
