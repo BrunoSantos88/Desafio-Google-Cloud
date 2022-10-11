635bbcfa59e66e7fd5676bddff64c635f1b35857
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-10 23:35:32 -0300

    terraform build

--- intancia.tf
+++ intancia.tf
@@ -1,7 +1,12 @@
-resource "google_compute_instance" "monitoramento" {
-  name         = "monitoramento-server"
+provider "google" {
+  project = "bootcamp-363315"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "servidorweb" {
+  name         = "frontend"
   machine_type = "e2-standard-4"
-  zone         = "southamerica-east1-b"
+  zone         = "us-central1-c"
 
   boot_disk {
     initialize_params {
