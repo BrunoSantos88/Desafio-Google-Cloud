080a16c094dc7a91a1de30b9a053e9a541afbe35
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 10:37:32 -0300

    terraform outputs

--- network.tf
+++ network.tf
@@ -4,3 +4,11 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = true
   mtu                     = 1460
 }
+
+
+resource "google_compute_subnetwork" "network-with-private-ip-ranges" {
+  name          = "rede-subnetwork"
+  ip_cidr_range = "192.10.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
