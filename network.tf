cc11e075afd49cc19d3a2cd2a723f4f8bc79fc7d
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 10:45:37 -0300

    terraform build

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = "my-project-name"
+  project                 = "bootcamp-363315"
   name                    = "vpc-network"
   auto_create_subnetworks = true
   mtu                     = 1460
