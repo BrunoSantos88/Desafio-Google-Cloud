902ba80b638d234a037bbd9e8366ae61037aee72
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-11 10:16:42 -0300

    terraform outputs

---
+++ security.tf
@@ -0,0 +1,22 @@
+resource "google_compute_firewall" "rules" {
+project       = "bootcamp-363315"
+  name        = "Frontend-security"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances frontend"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  source_tags   = ["jobs"]
+  target_tags   = ["frontend"]
+
+}
